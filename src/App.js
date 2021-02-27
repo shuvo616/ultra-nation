81eb958e9d604b6b7b5b50572be0bd9a4f6e0fab
unknown <shuvo616@gmail.com>
2021-02-27 15:18:57 +0600

    Another react project ultra nation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+    // .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
+      <h1>Country Loaded: {countries.length}</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Ultra Nation</p>
       </header>
     </div>
   );
